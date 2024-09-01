df7817752e323bd8d905057c11378c504d164ddf
sajjathossain <sajjathossain.official@gmail.com>
2024-09-01 21:41:35 +0600

    chore(navbar): modify layout and use navbar components

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -2,17 +2,24 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MobileNavComponent } from './mobile/mobile.component';
 import { TNavItem } from '@/lib/schemas';
+import { ContainerComponent } from '../container/container.component';
+import { DesktopNavComponent } from './desktop/desktop.component';
 
 @Component({
   standalone: true,
   selector: 'app-navbar',
-  imports: [RouterModule, MobileNavComponent],
+  imports: [
+    RouterModule,
+    MobileNavComponent,
+    ContainerComponent,
+    DesktopNavComponent,
+  ],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
   title = 'Products';
 
-  _navitems: TNavItem[] = [
+  navitems: TNavItem[] = [
     { title: 'Home', link: '/' },
     { title: 'New', link: '/create' },
     { title: 'About', link: '/about' },
