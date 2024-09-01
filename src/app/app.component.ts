148171238d7d8b1944d895db9fd3a2b37b10da03
sajjathossain <sajjathossain.official@gmail.com>
2024-09-01 19:00:29 +0600

    chore(navbar-component): update navbar import path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { NavbarComponent } from '@/components/shared';
 
 @Component({
   selector: 'app-root',
