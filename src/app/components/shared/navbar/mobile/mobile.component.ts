8a8b8b68aa5b5c3d061e0a0c992d1fce55750b0a
sajjathossain <sajjathossain.official@gmail.com>
2024-09-01 21:41:10 +0600

    chore(navbar/mobile): modify styles and ui

--- src/app/components/shared/navbar/mobile/mobile.component.ts
+++ src/app/components/shared/navbar/mobile/mobile.component.ts
@@ -1,18 +1,19 @@
 import { TNavItem } from '@/lib/schemas';
 import { Component, Input, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ContainerComponent } from '../../container/container.component';
 
 @Component({
   standalone: true,
   templateUrl: './mobile.component.html',
   selector: 'app-mobile-nav',
-  imports: [RouterModule],
+  imports: [RouterModule, ContainerComponent],
 })
 export class MobileNavComponent {
   @Input({
     required: true,
   })
-  _navitems: TNavItem[] = [];
+  navitems: TNavItem[] = [];
   _isOpen = signal(false);
 
   _toggle() {
