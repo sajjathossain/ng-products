e43d368b3399c3cd4fb9ab8c0050ffa0086b9055
sajjathossain <sajjathossain.official@gmail.com>
2024-09-02 18:00:41 +0600

    chore(container.component): modify template

--- src/app/components/shared/container/container.component.ts
+++ src/app/components/shared/container/container.component.ts
@@ -4,19 +4,10 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-container',
-  template: `
-    <span
-      class="block relative px-4 md:px-0 mx-0 md:mx-auto w-full md:w-2/3 h-full"
-    >
-      <ng-content />
-    </span>
-  `,
+  template: ` <ng-content /> `,
   styles: `
     :host {
-      display: block;
-      position: relative;
-      width: 100%;
-      height: 100%;
+      @apply block relative px-4 md:px-0 mx-0 md:mx-auto w-full h-full md:w-2/3 overflow-y-auto md:overflow-y-visible;
     }
   `,
   imports: [NgTemplateOutlet],
