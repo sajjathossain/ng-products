734f63a5310f13d0627a90f70f4e92d82b26c267
sajjathossain <sajjathossain.official@gmail.com>
2024-09-02 08:43:44 +0600

    chore(app.component): add toggle form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@/components/shared';
+import { ProductsFormComponent } from './components/products/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ProductsFormComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'tiger-it-task';
+  showForm = signal(false);
+  @Output() toggle = new EventEmitter<boolean>();
+
+  toggleForm() {
+    this.showForm.set(!this.showForm());
+  }
 }
