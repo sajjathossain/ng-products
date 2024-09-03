89ba52e923d6b8d654ffe1513a35b6b1b9b65a61
sajjathossain <sajjathossain.official@gmail.com>
2024-09-03 23:12:06 +0600

    chore(schemas/communication): update interfaces

--- src/app/lib/schemas/communication.ts
+++ src/app/lib/schemas/communication.ts
@@ -1,10 +1,19 @@
 export interface IUpdateProductBehaviorSubject {
   updateId: string | undefined;
+  deleteId?: null;
+}
+
+export interface IDeleteProductBehaviorSubject {
+  deleteId: string | undefined;
+  updateId?: null;
 }
 
 export interface IToggleFormBehaviorSubject {
   toggle: boolean;
 }
 
-export type TProductBehaviorSubject = IUpdateProductBehaviorSubject | null;
+export type TProductBehaviorSubject =
+  | IUpdateProductBehaviorSubject
+  | IDeleteProductBehaviorSubject
+  | null;
 export type TFormBehaviorSubject = IToggleFormBehaviorSubject | null;
