b47f2715d0349667cb31f465843d77b8253570d1
sajjathossain <sajjathossain.official@gmail.com>
2024-09-03 23:18:17 +0600

    chore(table-view.component): add action buttons

--- src/app/components/products/product-list/table-view/table-view.component.ts
+++ src/app/components/products/product-list/table-view/table-view.component.ts
@@ -2,52 +2,59 @@ import { ProductDocType } from '@/db/product.schema';
 import { DatePipe } from '@angular/common';
 import { Component, Input, WritableSignal } from '@angular/core';
 import {
-  ColumnDef,
+  FlexRenderComponent,
   FlexRenderDirective,
   createAngularTable,
+  createColumnHelper,
   getCoreRowModel,
 } from '@tanstack/angular-table';
 import { RxDocumentData } from 'rxdb';
+import { ProductActionButtonsComponent } from '../action-buttons/action-buttons.component';
 
 type TProductType = RxDocumentData<ProductDocType>;
 
-const columns: ColumnDef<TProductType>[] = [
-  {
-    accessorKey: 'name',
-    header: 'Name',
-  },
-  {
-    accessorKey: 'category',
-    header: 'Category',
-  },
-  {
-    accessorKey: 'quantity',
-    header: 'Quantity',
-  },
-  {
-    accessorKey: 'price',
-    header: 'Total',
-    cell: ({ row }) =>
-      Number(row.getValue('quantity')) * Number(row.getValue('price')),
-  },
-  {
-    header: 'Actions',
-  },
-];
+const columnHelper = createColumnHelper<TProductType>();
 
 @Component({
   standalone: true,
   selector: 'app-products-table-view',
   templateUrl: './table-view.component.html',
-  imports: [FlexRenderDirective, DatePipe],
+  imports: [FlexRenderDirective, DatePipe, ProductActionButtonsComponent],
 })
 export class ProductsTableViewComponent {
   protected readonly datePipe = new DatePipe('en-US');
   @Input() products!: WritableSignal<TProductType[]>;
 
+  deaultColumns = [
+    columnHelper.accessor('name', {
+      header: 'Name',
+    }),
+    columnHelper.accessor('category', {
+      header: 'Category',
+    }),
+    columnHelper.accessor('quantity', {
+      header: 'Quantity',
+    }),
+    columnHelper.accessor('price', {
+      header: 'Total',
+      cell: ({ row }) =>
+        Number(row.getValue('quantity')) * Number(row.getValue('price')),
+    }),
+    columnHelper.display({
+      id: 'id',
+      header: 'Actions',
+      cell: (info) => {
+        const id = info.row.original.id;
+        return new FlexRenderComponent(ProductActionButtonsComponent, {
+          id,
+        });
+      },
+    }),
+  ];
+
   table = createAngularTable(() => ({
     data: this.products(),
-    columns,
+    columns: this.deaultColumns,
     getCoreRowModel: getCoreRowModel(),
     debugTable: true,
   }));
