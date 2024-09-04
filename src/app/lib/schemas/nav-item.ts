f890ad3ed4a8ef4e8cfcc2409121d972feef14c5
sajjathossain <sajjathossain.official@gmail.com>
2024-09-04 23:33:51 +0600

    chore(app): remove zod and unnecessary schemas

--- src/app/lib/schemas/nav-item.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const navItemSchema = z.object({
-  title: z.string().min(1, 'Title is required'),
-  link: z.string().min(1, 'Link is required'),
-});
-
-export type TNavItem = z.infer<typeof navItemSchema>;
